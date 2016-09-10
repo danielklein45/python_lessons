329b50f05b84b5d77928d0c2ef1b0cfb769a46d3
daniel klein <danielklein45@gmail.com>
2016-09-10 12:40:13 +0300

    Ex3 question 2

--- Excercise3/Excercise3.py
+++ Excercise3/Excercise3.py
@@ -43,8 +43,85 @@ def test4_map_pl(z):
     return map_pl(ls, (lambda x: abs(x) < z), (lambda x: x+x))
 
 
+# Question 2 - part 1
+def f321_algorithm(n):
+    if n % 2 == 0:
+        z = n // 2
+    else:
+        z = n * 3 + 1
+    return z
+
+
+def list321(n):
+
+    def f321_generator(n):
+        nonlocal number_of_steps
+        if n == 1:
+            yield 1
+            number_of_steps += 1
+            return
+        yield n
+        number_of_steps += 1
+        z = f321_algorithm(n)
+        for w in f321_generator(z):
+            yield w
+
+    number_of_steps = 0
+    g = f321_generator(n)
+    for x in g:
+        pass
+    return number_of_steps
+
+
+# Question 2 - part 2
+def f321_with_cache():
+    dict_nums = {}
+    number_of_steps = 0
+
+    def cache321(n):
+        nonlocal dict_nums
+        nonlocal number_of_steps
+
+        # reached the end of algorithm
+        if n == 1:
+            number_of_steps += 1
+            return 0
+        # check if n in cache
+        if n in dict_nums:
+            return dict_nums[n]
+        # n is not in cache
+        else:
+            z = f321_algorithm(n)
+            dict_nums[n] = cache321(z)
+            number_of_steps += 1
+
+        return number_of_steps
+
+    # return the dictionary
+    cache321.get_cache = dict_nums
+    return cache321
+
+
 if __name__ == '__main__':
+    # test question1
+    print("Question1")
     print(test1_map_pl())
     print(test2_map_pl())
     print(test3_map_pl())
     print(test4_map_pl(7))
+    print("Question2 - part1")
+    # test question2 - part 1 - return 17
+    print(list321(7))
+    # test question2 - part 2
+    print("Question2 - part2")
+    f321 = f321_with_cache()
+    # print 10 and create cache
+    print(f321(13))
+    d = f321.get_cache
+    print(sorted(d.items()))
+    # print 17 and append cache
+    print(f321(7))
+    d = f321.get_cache
+    print(sorted(d.items()))
+
+
